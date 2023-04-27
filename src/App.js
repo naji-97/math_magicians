9e15d3abc8b8e45588cadc8c5e573af433cc588e
Naji-97 <elbairabennaji@gmail.com>
2023-04-27 16:17:34 +0100

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Claculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
