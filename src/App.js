19a14d4c372cafb146ba9627dce6975439bda7e3
Naji-97 <elbairabennaji@gmail.com>
2023-05-02 14:23:41 +0100

    Add pages and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import Calculator from './components/Claculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Quote.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles/Quote.css';
+
+function Quote() {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: {
+        'X-Api-Key': 'HmXWPPlh8e3KzGUAtzN6Dg==BncHWBjQ4yEvHwrB',
+      },
+    })
+      .then((response) => {
+        setQuote(response.data[0]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div className="center">Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error.message}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {quote && (
+        <div className="quote">
+          <p>{quote.quote}</p>
+          <p>
+            —
+            <em style={{ fontStyle: 'italic' }}>{quote.author}</em>
+          </p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Quote;
